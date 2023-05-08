36922ccd61971846e5fe77c93a4c46e384d90be2
federicofucci21 <federicofucci@hotmail.com>
2023-05-08 00:52:14 -0300

    minor changes

--- src/components/Footer/Copyrights.tsx
+++ src/components/Footer/Copyrights.tsx
@@ -4,9 +4,12 @@ export const Copyright = () => {
   return (
     <h5 className={style.copy}>
       {"Copyright © " + new Date().getFullYear()}{" "}
-      <a href="/"> Seba.Kinesio</a>. Todos los
+      <a href="/"></a>. Todos los
       derechos reservados. | Diseñado por {" "}{" "}
-      <a href="/"> CaneDevs</a>
+      <a       
+      href="https://api.whatsapp.com/send?phone=5493416745943&text=Hola,%20quisiera%20consultar%20sobre%20"
+      target="_black"
+      > CaneDevs</a>
     </h5>
   );
 };
