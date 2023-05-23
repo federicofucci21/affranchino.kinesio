7ff60404c9ddfff5dcb1a06b2776305d6e6a9de6
federicofucci21 <federicofucci@hotmail.com>
2023-05-22 23:32:24 -0300

    landing css

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,21 +1,46 @@
 import { Link } from "react-router-dom";
 import style from "./Landing.module.css";
+import logo2 from "../../assets/IMG/logo2.jpeg";
 
 export default function LandingPage() {
   return (
     <div className={style.landing}>
-      <div  className={style.landingDiv}>
-        <div className={style.content}>
-          <h2>Affranchino.Kinesio</h2>
-          <h2>Affranchino.Kinesio</h2>
+      <div className={style.imgDiv}>
+        <img src={logo2} alt="IMG not found" />
+      </div>
+      <div className={style.landingDiv}>
+        <div className={style.landingDiv2}>
+          <div className={style.content}>
+            <h2>Affranchino.Kinesio</h2>
+            <h2>Affranchino.Kinesio</h2>
+          </div>
+          <Link to={"/home"} className={style.button}>
+            <span>INGRESAR</span>
+          </Link>
         </div>
-        <Link to={"/home"} className={style.button}>
-          <span>INGRESAR</span>
-        </Link>
       </div>
     </div>
   );
 }
+
+// import { Link } from "react-router-dom";
+// import style from "./Landing.module.css";
+
+// export default function LandingPage() {
+//   return (
+//     <div className={style.landing}>
+//       <div  className={style.landingDiv}>
+//         <div className={style.content}>
+//           <h2>Affranchino.Kinesio</h2>
+//           <h2>Affranchino.Kinesio</h2>
+//         </div>
+//         <Link to={"/home"} className={style.button}>
+//           <span>INGRESAR</span>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
 
 // export default function LandingPage() {
 
