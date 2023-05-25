d4a4120295507fb3e5960c0c74f918934ab6b266
federicofucci21 <federicofucci@hotmail.com>
2023-05-25 01:07:42 -0300

    about page

---
+++ src/assets/constants/cv.tsx
@@ -0,0 +1,116 @@
+export const studies = [
+  {
+    name: "Licenciatura en Kinesiología y Fisiatría ",
+    date: 2015,
+    university: "Universidad Nacional de San Martín",
+    city: "Rosario, Santa Fe, Argentina",
+  },
+];
+
+export const courses = [
+  {
+    name: "Introducción a la Osteopatía y Terapias Manuales",
+    date: 2015,
+    university: "Universidad Abierta Interamericana",
+    city: "Rosario, Santa Fe, Argentina",
+  },
+  {
+    name: "Terapia Sacrocraneal I - Osteopatía Craneal",
+    date: 2016,
+    university: "Upledger Institute International",
+    city: "CABA, Argentina",
+  },
+  {
+    name: "Introducción a la RCF - (Reeducación corporal funcional)",
+    date: 2018,
+    university: "Universidad Abierta Interamericana",
+    city: "Rosario, Santa Fe, Argentina",
+  },
+  {
+    name: "Acupuntura 1",
+    date: 2019,
+    university: "IMADA, Instituto Medico Argentino de Acupuntura",
+    city: "CABA, Argentina",
+  },
+  {
+    name: "Formación de base en RPG. (Souchard) Nivel 1 (columna, MMSS Y MMII)",
+    date: 2021,
+    university: "",
+    city: "CABA, Argentina",
+  },
+  {
+    name: "Emociones y medicina china",
+    date: 2022,
+    university: "Escuela de shiatsu somático",
+    city: "CABA, Argentina",
+  },
+  {
+    name: "Seminario: “Fascias” ",
+    date: 2023,
+    university: "Escuela de shiatsu somático",
+    city: "CABA, Argentina",
+  },
+];
+
+export const work_experience = [
+  {
+    puesto: "Kinesiólogo",
+    date: "08/2021 - 02/2022",
+    institution: "Consultorio kinesiológico “Marcela Marengo”",
+    area: "Consultorio (FKT Y RPG)",
+  },
+  {
+    puesto: "Kinesiólogo",
+    date: "11/2018 – 02/2022",
+    institution: "Consultorio “Kinesio Alto Palermo””",
+    area: "Consultorio (FKT Y RPG)",
+  },
+  {
+    puesto: "Kinesiólogo",
+    date: "11/2017 – 11/2018",
+    institution: "Consultorio “KINECENTRO” ",
+    area: "Consultorio (FKT Y RPG)",
+  },
+  {
+    puesto: "Kinesiólogo",
+    date: "4/2018 – 2/2018",
+    institution: "Consultorio “BIOKINESIS”",
+    area: "Consultorio. Atención de pacientes neurológicos y traumatológicos",
+  },
+  {
+    puesto: "Kinesiólogo",
+    date: "10/2017 – 09/2018",
+    institution: "Consultorio “REKIOS”",
+    area: "Consultorio. Atención de pacientes en el área de traumatología y deportología",
+  },
+  {
+    puesto: "Kinesiólogo",
+    date: "05/2016 - 04/2017",
+    institution: "Clínica Privada “BECCAR“",
+    area: "Consultorio. Atención de pacientes en área de traumatología, neurología, gerontología, etc. Sala externa: atención de pacientes, rehabilitación motriz y respiratoria",
+  },
+  {
+    puesto: "Kinesiólogo",
+    date: "05/2015 - 08/2015",
+    institution: "Consultorios Médicos “VIDA”",
+    area: "Consultorio. Atención a pacientes en áreas de traumatología, neurología, pediatría, respiratorio, gerontología, etc",
+  },
+  {
+    puesto: "Kinesiólogo",
+    date: "07/2014 – 04/2015",
+    institution: "Consultorio “Yanina Chávez”",
+    area: "Consultorio. Atención a pacientes en áreas de Traumatología, neurología, pediatría, respiratorio, gerontología, etc",
+  },
+  {
+    puesto: "Kinesiólogo",
+    date: "02/2014 – 06/2014",
+    institution: "Gimnasio “CRAF”",
+    area: "Consultorio – Gimnasio. Atención a pacientes en consultorio y gimnasio en áreas de Traumatología, deportología, geriatría, etc",
+  },
+  {
+    puesto: "Kinesiólogo",
+    date: "02/2013 – 01/2014",
+    institution: "Gimnasio “CEFIDE”",
+    area: "Consultorio – Gimnasio. Atención a pacientes en consultorio y gimnasio en áreas de Traumatología, deportología, geriatría, etc",
+  },
+];
--- src/pages/About/about.tsx
+++ src/pages/About/about.tsx
@@ -1,9 +1,45 @@
 import style from "./about.module.css";
+import sebaloco from "../../assets/IMG/Sebaloco.png";
+import { studies, courses, work_experience } from "../../assets/constants/cv";
 
 export default function About() {
   return (
     <nav className={style.about}>
-        ABOUT
+      <div className={style.divAbout}>
+        <section className={style.header}>
+          <div>
+            <img src={sebaloco} alt="IMG not found" />
+          </div>
+          <h2>Lic. Sebastián Affranchino</h2>
+          <h4>M.N. 15463</h4>
+        </section>
+        <section className={style.scroll}>
+          <div className={style.studies}>
+              <h1>Preparación Academica</h1>
+            <div>
+              {studies.map((e) => (
+                <li><h5>{e.name}</h5><h5>{e.date}</h5><h5>{e.university}</h5><h5>{e.city}</h5></li>
+              ))}
+            </div>
+          </div>
+          <div className={style.courses}>
+              <h1>Formación Profesional</h1>
+            <div>
+              {courses.map((e) => (
+                <li><h5>{e.name}</h5><h5>{e.date}</h5><h5>{e.university}</h5><h5>{e.city}</h5></li>
+              ))}
+            </div>
+          </div>
+          <div className={style.courses}>
+              <h1>Experiencia Laboral</h1>
+            <div>
+              {work_experience.map((e) => (
+                <li><h5>Institución: {e.institution}</h5><h5>Puesto: {e.puesto}</h5><h5> Fecha: {e.date}</h5><h5>Area: {e.area}</h5></li>
+              ))}
+            </div>
+          </div>
+        </section>
+      </div>
     </nav>
   );
 }
