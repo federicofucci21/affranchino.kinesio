a70fa86a47f249a954090027ab4795cc17c1bd16
federicofucci21 <federicofucci@hotmail.com>
2023-07-02 20:50:07 -0300

    change facebook button to whtasapp button on fotter

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { Copyright } from "./Copyrights";
 import style from "./Footer.module.css";
-import { BsFacebook, BsInstagram } from "react-icons/bs";
+import { BsInstagram, BsWhatsapp } from "react-icons/bs";
 import logo from "../../assets/IMG/logo.jpeg"
 import { useLocation } from "react-router-dom";
 
@@ -28,12 +28,12 @@ export function Footer() {
                 target="_blank"
                 rel="noreferrer"
               >
-                <BsFacebook className={style.socialIcon} />
+                <BsWhatsapp className={style.icon} />
               </a>
               <a
-                href="https://www.instagram.com/affranchino.kinesio/"
-                target="_blank"
-                rel="noreferrer"
+                href="https://api.whatsapp.com/send?phone=5491123308733&text=Hola,%20quisiera%20consultar%20sobre%20"
+                id="btn-wsp"
+                target="_black"
               >
                 <BsInstagram className={style.socialIcon} />
               </a>
