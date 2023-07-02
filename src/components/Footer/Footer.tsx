8b87608e2849a700aed2dfaf6177975ca179f7f3
federicofucci21 <federicofucci@hotmail.com>
2023-07-02 20:53:31 -0300

    fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -24,16 +24,16 @@ export function Footer() {
           <div className={style.social}>
             <div className={style.redes}>
               <a
-                href="https://www.instagram.com/affranchino.kinesio/"
-                target="_blank"
-                rel="noreferrer"
+                href="https://api.whatsapp.com/send?phone=5491123308733&text=Hola,%20quisiera%20consultar%20sobre%20"
+                id="btn-wsp"
+                target="_black"
               >
                 <BsWhatsapp className={style.socialIcon} />
               </a>
               <a
-                href="https://api.whatsapp.com/send?phone=5491123308733&text=Hola,%20quisiera%20consultar%20sobre%20"
-                id="btn-wsp"
-                target="_black"
+                href="https://www.instagram.com/affranchino.kinesio/"
+                target="_blank"
+                rel="noreferrer"
               >
                 <BsInstagram className={style.socialIcon} />
               </a>
